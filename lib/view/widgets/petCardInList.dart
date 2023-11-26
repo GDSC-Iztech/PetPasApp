19a641d3bfabb1fe53d1eea0316719ad2b0eff73
Mert <mertkarahan955@gmail.com>
2023-11-27 01:48:45 +0300

    hayvan listesi kabaca tamamlandi

---
+++ lib/view/widgets/petCardInList.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:petpas/constants/constants.dart';
+
+class PetCardInList extends StatefulWidget {
+  final String name;
+
+  const PetCardInList({Key? key, required this.name}) : super(key: key);
+
+  @override
+  State<PetCardInList> createState() => _PetCardInListState();
+}
+
+class _PetCardInListState extends State<PetCardInList> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: appColor.blue,
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: ListTile(
+        leading: const CircleAvatar(
+          radius: 30.0,
+          backgroundImage: NetworkImage(
+            'https://placekitten.com/100/100', // Replace with your pet image URL
+          ),
+        ),
+        title: Text(widget.name, style: textStyles.headerTextStyle),
+        trailing: IconButton(
+          icon: const Icon(
+            Icons.pets,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            // hangi hayvana basilirsa onun pasaportuna gidecek
+          },
+        ),
+      ),
+    );
+  }
+}
